e61ff56af2d58107dec028c4e6f9ec62ffe9ac85
Brian <mayak13@yahoo.com>
2018-01-07 13:57:01 -0800

    brain freeze

---
+++ Python/Python Fundamentals/String_and_Lists.py
@@ -0,0 +1,25 @@
+'''#Find and Replace
+words = "It's thanksgiving day. It's my birthday,too!"
+print words.find("day")
+print words.replace("day", "month")
+
+#Min and Max
+x = [2,54,-2,7,12,98]
+print min(x)
+print max(x)
+
+#First and Last
+x = ["hello",2,54,-2,7,12,98,"world"]
+print x[0]
+print x[len(x)-1]
+'''
+#New List
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+x.sort()
+print x
+newarr = []
+temp = x[:len(x)/2]
+print temp
+newarr = x[len(x)/2:]
+newarr.insert(0,temp)
+print newarr
---
+++ Python/Python Fundamentals/hello_world.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+x = "Hello Python"
+print x
+y = 42
+print y
+
+x = '1'
+y = 2
+print x + str(y)
