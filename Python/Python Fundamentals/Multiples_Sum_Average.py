c4b8832e63da420683deccf34482c65af8d1d83e
Brian <mayak13@yahoo.com>
2018-01-07 16:58:51 -0800

    yo

---
+++ Python/Python Fundamentals/Multiples_Sum_Average.py
@@ -0,0 +1,17 @@
+#Multiples Part 1 (prints all of the numbers from 1 to 1000)
+for x in range(1,1001):
+	print x
+#Multiples Part 2 (prints all of the multiples of 5 from 5 to 1000)
+for x in range(5,1001,5):
+	print x
+#Sum list (sums up all of the list)
+a = [1,2,5,10,255,3]
+print sum(a)
+
+#Average List (take the sum of the list and divide it by the length to get the average)
+a = [1,2,5,10,255,3]
+print sum(a)/len(a)
+
+
+
+
